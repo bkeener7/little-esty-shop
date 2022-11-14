beff632db418c3b051561e8fedefcbe9c9893a36
bkeener <bkeener7@gmail.com>
2022-11-14 08:47:05 -0700

    Finish US2

--- app/controllers/merchant_discounts_controller.rb
+++ app/controllers/merchant_discounts_controller.rb
@@ -10,6 +10,24 @@ class MerchantDiscountsController < ApplicationController
 
   def new
     @merchant = Merchant.find(params[:merchant_id])
-    @discount = @merchant.bulk_discounts.new
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    new_discount = @merchant.bulk_discounts.new(bulk_discount_params)
+
+    if new_discount.save
+      flash[:message] = 'Discount successfully created'
+      redirect_to merchant_discounts_path(@merchant)
+    else
+      flash[:error] = 'Required content missing or invalid'
+      redirect_to new_merchant_discount_path(@merchant)
+    end
+  end
+
+  private
+
+  def bulk_discount_params
+    params.permit(:percentage_discount, :quantity_threshold)
   end
 end
