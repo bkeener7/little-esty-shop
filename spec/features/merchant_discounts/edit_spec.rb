591fbe479831738a8df0206c59694b53cc5bfbf4
bkeener <bkeener7@gmail.com>
2023-05-16 16:07:01 -0600

    Remove repeated test and refactor

--- spec/features/merchant_discounts/edit_spec.rb
+++ spec/features/merchant_discounts/edit_spec.rb
@@ -1,35 +1,8 @@
 require 'rails_helper'
 
-RSpec.describe 'bulk discounts edit page' do
+RSpec.describe 'Bulk Discounts Edit Page' do
   let!(:merchant1) { create(:merchant) }
   let!(:merchant2) { create(:merchant) }
-
-  let!(:customer1) { create(:customer) }
-  let!(:customer2) { create(:customer) }
-  let!(:customer3) { create(:customer) }
-  let!(:customer4) { create(:customer) }
-  let!(:customer5) { create(:customer) }
-
-  let!(:invoice1) { create(:invoice, customer: customer1) }
-  let!(:invoice2) { create(:invoice, customer: customer2) }
-  let!(:invoice3) { create(:invoice, customer: customer3) }
-  let!(:invoice4) { create(:invoice, customer: customer4) }
-  let!(:invoice5) { create(:invoice, customer: customer5) }
-
-  let!(:item1) { create(:item, merchant: merchant1) }
-  let!(:item2) { create(:item, merchant: merchant1) }
-  let!(:item3) { create(:item, merchant: merchant2) }
-  let!(:item4) { create(:item, merchant: merchant2) }
-
-  let!(:invoice_item1) { create(:invoice_item, item: item1, invoice: invoice1) }
-  let!(:invoice_item2) { create(:invoice_item, item: item2, invoice: invoice2) }
-  let!(:invoice_item3) { create(:invoice_item, item: item3, invoice: invoice3) }
-  let!(:invoice_item4) { create(:invoice_item, item: item4, invoice: invoice4) }
-
-  let!(:transaction1) { create(:transaction, invoice: invoice1) }
-  let!(:transaction2) { create(:transaction, invoice: invoice2) }
-  let!(:transaction3) { create(:transaction, invoice: invoice3) }
-  let!(:transaction4) { create(:transaction, invoice: invoice4) }
 
   let!(:bulk_discount1) { create(:bulk_discount, merchant: merchant1) }
   let!(:bulk_discount2) { create(:bulk_discount, merchant: merchant1) }
@@ -40,14 +13,10 @@ RSpec.describe 'bulk discounts edit page' do
     visit edit_merchant_discount_path(merchant1, bulk_discount1)
   end
 
-  describe 'when I visit my bulk discounts show page' do
-    it 'goes to the bulk discounts show page' do
-      expect(current_path).to eq(edit_merchant_discount_path(merchant1, bulk_discount1))
-    end
-
-    it 'has a form with current discounts pre-populated' do
+  describe 'When I visit the bulk discount edit page' do
+    it 'displays the form with current discounts pre-populated' do
       within '#edit_form' do
-        expect(page).to have_selector(:css, 'form')
+        expect(page).to have_selector('form')
         expect(page).to have_content('Percentage Discount:')
         expect(page).to have_field(:percentage_discount, with: bulk_discount1.percentage_discount)
         expect(page).to have_content('Quantity Threshold:')
@@ -56,7 +25,7 @@ RSpec.describe 'bulk discounts edit page' do
       end
     end
 
-    it 'allows you to update both fields and takes you back to the show page with attributes updated' do
+    it 'updates both fields and redirects to the show page with updated attributes' do
       within '#edit_form' do
         fill_in :percentage_discount, with: 20
         fill_in :quantity_threshold, with: 20
@@ -71,22 +40,7 @@ RSpec.describe 'bulk discounts edit page' do
       end
     end
 
-    it 'allows you to update both fields and takes you back to the show page with attributes updated' do
-      within '#edit_form' do
-        fill_in :percentage_discount, with: 20
-        fill_in :quantity_threshold, with: 20
-        click_button('Update Discount')
-      end
-
-      expect(current_path).to eq(merchant_discount_path(merchant1, bulk_discount1))
-
-      within '#discount_details' do
-        expect(page).to have_content('Percentage Discount: 20%')
-        expect(page).to have_content('Quantity Threshold: 20')
-      end
-    end
-
-    it 'allows you to update one field and takes you back to the show page with attributes updated' do
+    it 'updates one field and redirects to the show page with updated attributes' do
       within '#edit_form' do
         fill_in :percentage_discount, with: 20
         click_button('Update Discount')
@@ -100,7 +54,7 @@ RSpec.describe 'bulk discounts edit page' do
       end
     end
 
-    it 'returns an error if content is missing from the form and redirects back to edit page' do
+    it 'returns an error if content is missing from the form and redirects back to the edit page' do
       within '#edit_form' do
         fill_in :percentage_discount, with: ''
         fill_in :quantity_threshold, with: 20
