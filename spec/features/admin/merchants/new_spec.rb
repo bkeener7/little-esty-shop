e7c518d32b82f5b38f60c78ea6f52e1b898681e9
bkeener <bkeener7@gmail.com>
2023-05-16 15:10:03 -0600

    Update admin/merchants test files for consistency

--- spec/features/admin/merchants/new_spec.rb
+++ spec/features/admin/merchants/new_spec.rb
@@ -2,19 +2,18 @@ require 'rails_helper'
 
 RSpec.describe 'Create new merchant' do
   it 'has a link on the admin dashboard form to create a new merchant and the status is set to disabled by default' do
-    visit '/admin/merchants'
+    visit admin_merchants_path
 
     within('#admin_dashboard') do
       expect(page).to have_link('New Merchant')
-
-      click_on 'New Merchant'
+      click_link 'New Merchant'
     end
 
-    expect(current_path).to eq('/admin/merchants/new')
+    expect(current_path).to eq(new_admin_merchant_path)
 
-    fill_in :name, with: 'Banana Fran'
-    click_on 'Create Merchant'
-    expect(current_path).to eq('/admin/merchants')
+    fill_in 'Name', with: 'Banana Fran'
+    click_button 'Create Merchant'
+    expect(current_path).to eq(admin_merchants_path)
 
     within('#disabled_merchants') do
       expect(page).to have_content('Banana Fran')
@@ -22,16 +21,16 @@ RSpec.describe 'Create new merchant' do
   end
 
   it 'returns an error message if the name is not inputted' do
-    visit '/admin/merchants'
+    visit admin_merchants_path
 
     expect(page).to have_link('New Merchant')
-    click_on 'New Merchant'
+    click_link 'New Merchant'
 
-    expect(current_path).to eq('/admin/merchants/new')
+    expect(current_path).to eq(new_admin_merchant_path)
 
-    fill_in :name, with: nil
-    click_on 'Create Merchant'
-    expect(current_path).to eq('/admin/merchants/new')
+    fill_in 'Name', with: nil
+    click_button 'Create Merchant'
+    expect(current_path).to eq(new_admin_merchant_path)
     expect(page).to have_content('ERROR: Please enter a valid name.')
   end
 end
--- spec/features/admin/merchants/show_spec.rb
+++ spec/features/admin/merchants/show_spec.rb
@@ -1,27 +1,26 @@
 require 'rails_helper'
 
 RSpec.describe 'admin/merchants-show page' do
-  before :each do
-    @merchant_1 = Merchant.create!(name: 'Marvel')
-    @merchant_2 = Merchant.create!(name: 'D.C.')
-    @merchant_3 = Merchant.create!(name: 'Darkhorse')
-    @merchant_4 = Merchant.create!(name: 'Image')
-  end
+  let!(:merchant_1) { create(:merchant, name: 'Marvel') }
+  let!(:merchant_2) { create(:merchant, name: 'D.C.') }
+  let!(:merchant_3) { create(:merchant, name: 'Darkhorse') }
+  let!(:merchant_4) { create(:merchant, name: 'Image') }
 
   describe 'when I click on the name of the merchant I am taken to the merchant show page' do
-    it "each name links to an merchants' show page" do
-      visit '/admin/merchants'
-      expect(page).to have_link(@merchant_1.name)
-      expect(page).to have_link(@merchant_2.name)
-      expect(page).to have_link(@merchant_3.name)
-      expect(page).to have_link(@merchant_4.name)
+    before { visit admin_merchants_path }
 
-      click_on "#{@merchant_1.name}"
+    it "each name links to a merchant's show page" do
+      expect(page).to have_link(merchant_1.name, href: admin_merchant_path(merchant_1))
+      expect(page).to have_link(merchant_2.name, href: admin_merchant_path(merchant_2))
+      expect(page).to have_link(merchant_3.name, href: admin_merchant_path(merchant_3))
+      expect(page).to have_link(merchant_4.name, href: admin_merchant_path(merchant_4))
 
-      expect(current_path).to eq("/admin/merchants/#{@merchant_1.id}")
+      click_link merchant_1.name
 
-      expect(page).to have_content(@merchant_1.name)
-      expect(page).to_not have_content(@merchant_2.name)
+      expect(current_path).to eq(admin_merchant_path(merchant_1))
+
+      expect(page).to have_content(merchant_1.name)
+      expect(page).not_to have_content(merchant_2.name)
     end
   end
 end
